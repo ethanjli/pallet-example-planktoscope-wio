c5068d8ad6404513a3f2b85fcf14757893ae7395
Ethan Li <lietk12@gmail.com>
2024-04-19 22:44:30 -0700

    Add debugging output

--- bridge/main.py
+++ bridge/main.py
@@ -6,16 +6,22 @@ import paho.mqtt.client as mqtt
 import serial
 
 def on_connect(client, userdata, flags, reason_code, properties):
+    print("connected!")
     client.subscribe("/#")
 
 def make_on_message(ser):
-    return lambda client, userdata, msg: ser.write(f"{msg.topic}: {msg.payload}\n".encode("utf-8"))
+    def on_message(client, userdata, msg):
+        print(f"{msg.topic}: {msg.payload}")
+        ser.write(f"{msg.topic}: {msg.payload}\n".encode("utf-8"))
+    return on_message
 
 if __name__ == "__main__":
+    print("connecting to /dev/ttyACM0...")
     ser = serial.Serial("/dev/ttyACM0")
     mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     mqttc.on_connect = on_connect
     mqttc.on_message = make_on_message(ser)
 
+    print("connecting to mqtt...")
     mqttc.connect("host.docker.internal", 1883, 60)
     mqttc.loop_forever()
